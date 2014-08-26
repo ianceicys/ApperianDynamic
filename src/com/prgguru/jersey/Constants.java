f5b8653f953098c06d3bbef35dbfc3e4b9efbbff
ianceicys <Zeo01@hotmail.com>
2014-08-26 12:14:09 -0400

    Adding Java Project
    Adding Java Project

---
+++ src/com/prgguru/jersey/Constants.java
@@ -0,0 +1,9 @@
+package com.prgguru.jersey;
+//Change these parameters according to your DB
+public class Constants {
+	public static String dbClass = "com.mysql.jdbc.Driver";
+	private static String dbName= "users";
+	public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+	public static String dbUser = "root";
+	public static String dbPwd = "password";
+}
